7e8bc005b5d0429fc1eb8f953bbbf2e4a7d400af
gooscyora <adriangusciora98@gmail.com>
2020-01-03 15:47:28 +0100

    Add Picutre Preview to Car Create View

--- ShopWebsite/Models/Car.cs
+++ ShopWebsite/Models/Car.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShopWebsite.Models
@@ -23,5 +24,8 @@ namespace ShopWebsite.Models
 
         [ForeignKey("CarTypeId")]
         public virtual CarType CarType { get; set; }
+
+        [NotMapped] //Saving files locally
+        public IFormFile ImageUpload { get; set; }
     }
 }
