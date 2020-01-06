d9c7e939d4dd5c0cd273d6f8959ba105c025ac92
gooscyora <adriangusciora98@gmail.com>
2020-01-06 21:22:38 +0100

    Added Categories Navigation

---
+++ ShopWebsite/Infrastructure/CategoriesViewComponent.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopWebsite.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopWebsite.Infrastructure
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly ShopDbContext _context;
+
+        public CategoriesViewComponent(ShopDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+            return View(pages);
+        }
+
+        private Task<List<CarType>> GetPagesAsync()
+        {
+            return _context.CarTypes.ToListAsync();
+        }
+
+
+    }
+}
